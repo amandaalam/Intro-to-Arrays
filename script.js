843de80f2bc257027a85b4141ae02cec49da8fba
Amanda Lam <amandaalam21@gmail.com>
2020-06-30 20:38:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// WARM UP
+// Declare a variable for every scholar in the zoom. The variable names should be something like scholar1, scholar2, etc. and the values should be strings of their first names. Be ready to share your code!
+ 
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// DEFINITIONS
+// Arrays: 
+// An array of strings:
+// An array of numbers: 
+// Index: 
+
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// TRY IT!
+// Create a variable that stores an array of at least 4 strings - you choose what the content is about. The variable name should describe the type of data those 4 strings hold.
+
+
+
+//Write a series of console.log() statements: print out the first element, the last element, and then the second element.
+
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// CHANGING ELEMENTS
+// Updating: 
+
+
+// Adding:
+
+
+// Removing:
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// TRY IT!
+//For this, you will use the array you wrote in the previous Try It.
+//Practice accessing specific elements. Make sure to console.log() to verify you are accessing what you think you are.
+
+
+//Now, add two new elements into your array. Use a console.log() to make sure they have been added.
+
+
+//Lastly, remove at least two elements from your array. Again, make sure they have been successfully removed by printing to the console.
+
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// RANDOM ELEMENTS
+//Math.random() - returns a random decimal between 0 and 1
+//Math.floor(number) - returns the value of a number rounded down to the nearest integer
+
+
+// EXAMPLE: 
+// Why is this useful??
+var students = ["Leta", "Ellen Mary", "Amanda", "Megan", "Amy", "Sarah", "Robyn", "Colin", "Rachel", "Allison", "Ruby", "Maile", "Ishika", "Mary", "Christie", "Emmie", "Aurora", "Tori", "Juliana", "Zoe"]
+
+
+
+// ⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// PRACTICE
+//Declare a variable called following that stores an array of your top five favorite accounts to follow on social media, in strings.
+//Change the value of at least one of the elements in the array
+
+//Add a new account to the array
+
+//Remove the last account from the array
+
+//Print the value of the third element of the array
+
+//Change the value of another element in the array
+
+//Add another account to the array
+
+//Print the value of the first element of the array
+
+//Print one account to the console, at random.
+
+
+
+// 🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶🌶
+// It's getting SPICY!
+// Write a function that takes in one argument, a string. If the string is "happy", output a randomly generated sentence about being happy. If the string is "ok", output a randomly generated sentence about being ok. If the string is "sad", output a randomly generated sentence to cheer someone up. You will need to use a function, conditional, multiple arrays, and have to generate a random number.
